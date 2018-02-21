5393968f1c6771dd7422a6cde3e8a123703fde66
Michael van der Werve <mvdwerve@outlook.com>
2018-02-21 14:53:28 +0100

    refactor of the second assignment

--- labs-master/Lab1_Photometric_Color/colorspace/main.m
+++ labs-master/Lab1_Photometric_Color/colorspace/main.m
@@ -6,25 +6,25 @@ close all
 
 I = imread('peppers.png');
 figure(1);
-J = ConvertColorSpace(I,'opponent',0);
- figure(2);
+J = ConvertColorSpace(I, 'opponent', 0);
+figure(2);
 % close all
-J = ConvertColorSpace(I,'rgb',0);
+J = ConvertColorSpace(I, 'rgb', 0);
 figure(3);
 % close all
-J = ConvertColorSpace(I,'hsv',0);
+J = ConvertColorSpace(I, 'hsv', 0);
 figure(4);
 % close all
-J = ConvertColorSpace(I,'ycbcr',0);
+J = ConvertColorSpace(I, 'ycbcr', 0);
 figure(5);
 % close all
-J = ConvertColorSpace(I,'gray',1);
+J = ConvertColorSpace(I, 'gray', 1);
 figure(6);
 % close all
-J = ConvertColorSpace(I,'gray',2);
+J = ConvertColorSpace(I, 'gray', 2);
 % close all
 figure(7);
-J = ConvertColorSpace(I,'gray',3);
+J = ConvertColorSpace(I, 'gray', 3);
 % close all
 figure(8);
-J = ConvertColorSpace(I,'gray',4);
+J = ConvertColorSpace(I, 'gray', 4);
--- labs-master/Lab1_Photometric_Color/colorspace/rgb2normedrgb.m
+++ labs-master/Lab1_Photometric_Color/colorspace/rgb2normedrgb.m
@@ -1,17 +1,5 @@
 function [output_image] = rgb2normedrgb(input_image)
-% converts an RGB image into normalized rgb
-
-    output=zeros(size(input_image));
-    [h , w , d]=size(input_image);
-    for i=1:h
-        for j= 1:w
-           output(i,j,1)=(input_image(i,j,1))/(input_image(i,j,1)+input_image(i,j,2)+input_image(i,j,3));
-           output(i,j,2)=(input_image(i,j,2))/(input_image(i,j,1)+input_image(i,j,2)+input_image(i,j,3));
-           output(i,j,3)=(input_image(i,j,3))/(input_image(i,j,1)+input_image(i,j,2)+input_image(i,j,3));
-        end
-        
-    end
-    
-    output_image=output;
+    % converts an RGB image into normalized rgb
+    output_image = input_image ./ sum(input_image, 3);
 end
 
