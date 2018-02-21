cb3ae6da87cad56518f00a6b1f394aa633486961
Michael van der Werve <mvdwerve@outlook.com>
2018-02-21 18:23:27 +0100

    added more plots and such... laptop fucking crashed

--- labs-master/Lab1_Photometric_Color/photometric/photometric_stereo.m
+++ labs-master/Lab1_Photometric_Color/photometric/photometric_stereo.m
@@ -3,20 +3,33 @@ close all;
 clc;
 
 % run it both with and without shadow trick
-photometric_stereo_single('./SphereGray5', 1, false, 0.005, 'column');
-% photometric_stereo_single('./SphereGray25', 1, false, 0.005, true);
-% photometric_stereo_single('./SphereGray5', 1, true, 0.005, true);
-% photometric_stereo_single('./SphereGray25', 1, true, 0.005, true);
+photometric_stereo_single('./SphereGray5', 1, false);
+close all;
+photometric_stereo_single('./SphereGray25', 1, false);
+close all;
+photometric_stereo_single('./SphereGray5', 1, true);
+close all;
+photometric_stereo_single('./SphereGray25', 1, true);
+close all;
 
 % run for the monkey
-% photometric_stereo_single('./MonkeyGray', 1, false, 0.005, true);
-% photometric_stereo_single('./MonkeyGray', 1, true, 0.005, true);
-% 
-% % run the color models for the sphere
-% photometric_stereo_color('./SphereColor', false, 0.005);
-% photometric_stereo_color('./SphereColor', true, 0.005);
+photometric_stereo_single('./MonkeyGray', 1, false);
+close all;
+photometric_stereo_single('./MonkeyGray', 1, true);
+close all;
+ 
+% run the color models for the sphere
+photometric_stereo_color('./SphereColor', false, 0.005);
+close all;
+photometric_stereo_color('./SphereColor', true, 0.005);
+close all;
 
 % run the color models for the monkey
-%photometric_stereo_color('./MonkeyColor', false, 0.005);
-%photometric_stereo_color('./MonkeyColor', true, 0.005, 'average');
-%photometric_stereo_color('./MonkeyColor', true, 0.005);
+photometric_stereo_color('./MonkeyColor', false, 0.005);
+close all;
+photometric_stereo_color('./MonkeyColor', true, 0.005);
+close all;
+photometric_stereo_color('./MonkeyColor', true, 0.005, 'average');
+close all;
+photometric_stereo_color('./MonkeyColor', true, 0.005, 'row');
+close all;
