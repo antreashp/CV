56592a3bd6f6f5a373d9dbab742dc4d152f48206
Michael van der Werve <mvdwerve@outlook.com>
2018-02-21 15:20:47 +0100

    refactor of third assignment

--- labs-master/Lab1_Photometric_Color/intrinsic_images/iid_image_formation.m
+++ labs-master/Lab1_Photometric_Color/intrinsic_images/iid_image_formation.m
@@ -1,27 +1,29 @@
-image=imread('ball.png');
-imR=imread('ball_reflectance.png');
-imS=imread('ball_shading.png');
-imR=im2double(imR);
+% load images and convert it to double values
+image = imread('ball.png');
+imR = imread('ball_reflectance.png');
+imS = imread('ball_shading.png');
+imR = im2double(imR);
+imS = im2double(imS);
 
-imS=im2double(imS);
-
-coloredImage=recolor(imR,imS);
+coloredImage = recolor(imR, imS);
 disp(size(coloredImage));
+
 figure(1);
 imshow(coloredImage);
+
 figure(2);
 imshow(imS);
+
 figure(3);
 imshow(imR);
+
 figure(4);
 imshow(image);
 
-
-
-[green, magenta]=recoloring(imR,imS);
+[green, magenta] = recoloring(imR, imS);
 
 figure(5);
 imshow(green);
+
 figure(6);
 imshow(magenta);
-
