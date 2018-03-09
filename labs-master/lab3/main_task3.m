b7799c218fd8af5a41758f1a508b1d6e3cce3e3e
antreashp <antreashp@gmail.com>
2018-03-09 17:19:11 +0100

    'lab3'

--- labs-master/lab3/main_task3.m
+++ labs-master/lab3/main_task3.m
@@ -21,3 +21,17 @@ prev_image=imread(imagefiles(1).name);
 [H,Gx,Gy]=harris_corner_detector(prev_image);
 Lucas_Kanade(H,imagefiles,nfiles,prev_image,'person_toy.avi',price);
    
+
+
+price=1 %how expensive the algorith is (0 is the most expensive number of images is the minimum price)
+%read images person_toy with feature detecting in each frame.
+imagefiles = dir('D:\Users\Andy\Downloads\Desktop\CV\labs-master\lab3\person_toy\*.jpg');      
+nfiles = length(imagefiles);
+disp(nfiles);
+prev_image=imread(imagefiles(1).name);
+
+[h,w,d]=size(prev_image);
+[H,Gx,Gy]=harris_corner_detector(prev_image);
+Lucas_Kanade(H,imagefiles,nfiles,prev_image,'pingpong.avi',price);
+   
+
